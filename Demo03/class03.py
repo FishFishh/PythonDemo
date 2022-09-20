162c0e0db05279fc0b4974e8db27301a0de11cfd
fish <fishsocute233@gmail.com>
2022-09-20 17:26:41 +0800

    1

---
+++ Demo03/class03.py
@@ -0,0 +1,9 @@
+day = str(input("今天什么日子:"))
+if day == "Valentine's Day":
+    print("buy one flower~")
+elif day == "Christmas Day":
+    print("buy one apple~")
+elif day == "Birthday":
+    print("buy one Birthday Cake~")
+else:
+    print("Every day is a day to love you~")
