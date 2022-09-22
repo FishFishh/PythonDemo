f1829ca7c8ce737d19b3f20b86197f4522bbee6e
fish <fishsocute233@gmail.com>
2022-09-22 10:52:57 +0800

    1

--- Demo03/class04.py
+++ Demo03/class04.py
@@ -1,9 +1,13 @@
 a = float(input("输入第一个数: "))
 b = float(input("输入第二个数: "))
 c = float(input("输入第三个数: "))
-if a > b and a > c:
-    print("最大数为%s" % a)
-elif b > a and b > c:
-    print("最大数为%s" % b)
-elif c > a and c > a:
-    print("最大数为%s" % b)
+if a > b:
+    if a > c:
+        print("最大数为%s" % a)
+    else:
+        print("最大数为%s" % c)
+else:
+    if b > c:
+        print("最大数为%s" % b)
+    else:
+        print("最大数为%s" % c)
--- Demo03/class05.py
+++ Demo03/class05.py
@@ -1,5 +1,9 @@
 money = float(input("输入销售额: "))
 if money <= 3000:
-    print("薪资等于%s" % money*2000)
+    print("薪资等于%s" % float(money*2000))
 elif 3000 < money <= 7000:
-    print("薪资等于%s" % money*2000)
+    print("薪资等于%s" % float(money*1.1*2000))
+elif 7000 < money <= 10000:
+    print("薪资等于%s" % float(money*1.15*2000))
+elif 10000 < money:
+    print("薪资等于%s" % float(money*1.2*2000))
