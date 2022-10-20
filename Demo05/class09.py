8516cc26b90037f285695c91a9195b76e6902332
fish <fishsocute233@gmail.com>
2022-10-20 10:21:44 +0800

    1

---
+++ Demo05/class09.py
@@ -0,0 +1,5 @@
+# 猴子偷桃
+peach = 1
+for i in range(9):
+    peach = (peach + 1) * 2
+print(peach)
---
+++ Demo08/classDemo.py
@@ -0,0 +1,19 @@
+list_num = [10, 20, 30]
+list_nu = []
+list_info = [" zhangsan", 2022123, 21, 75.5, True]
+print(list_info)['zhangsan', 2022123, 21, 75.5, True]
+# 虽然列表能够存储不同类型的数据，但是一般不适用这一特性
+list_info2 = [" lisi", 2022124, 22, 90, True]
+list_score = [61, 72, 73, 78]
+animal = ["tiger", "monkey", "snake"]
+animal.append(" elephant")
+print(animal)["tiger", "monkey", "snake","elephant"]
+animal.insert(2, "penguis")
+print(animal)["tiger", "monkey", "penguis","snake","elephant"]
+animal.extend(" birds")
+print(animal)["tiger", "monkey", "penguis","snake","elephant"]
+animal.extend(["birds"])
+print(ani
+print(value，....
+sep = '
+end = ' \n’，file=sys. stdout, flush=False)F
